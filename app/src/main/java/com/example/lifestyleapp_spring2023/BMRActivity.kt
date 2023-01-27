1725f891515ea075c10e4e82346c7b158914032a
Preston Hales <PrestongHales@gmail.com>
2023-01-27 15:00:53 -0700

    Create basic layout for the BMR activity

---
+++ app/src/main/java/com/example/lifestyleapp_spring2023/BMRActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lifestyleapp_spring2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BMRActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmractivity)
+    }
+}
