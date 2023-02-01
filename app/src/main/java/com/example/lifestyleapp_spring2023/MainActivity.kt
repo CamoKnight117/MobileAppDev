cbb34892b52648b4bbb8017e37db1e00178f0b46
CAMERON KNIGHT <u1070175@umail.utah.edu>
2023-01-31 20:53:32 -0700

    Refactored the main page and BMR page to function using fragments

--- app/src/main/java/com/example/lifestyleapp_spring2023/MainActivity.kt
+++ app/src/main/java/com/example/lifestyleapp_spring2023/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.lifestyleapp_spring2023
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, BMRActivity::class.java)
-        startActivity(intent)
+
+        //Find each frame layout, replace with corresponding fragment
+        //Find each frame layout, replace with corresponding fragment
+        val fTrans: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fTrans.replace(R.id.fragmentFrame1, bmr_page_fragment(), "Frag_1")
+        fTrans.commit()
     }
 }
