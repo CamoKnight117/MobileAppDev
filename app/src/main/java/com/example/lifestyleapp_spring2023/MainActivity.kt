cc352b50afa7fcb239374969cec8ad8a3aaf23db
Jacob Bullard <u0625839@gcloud.utah.edu>
2023-02-02 15:20:34 -0700

    successfull build and main activity framed

--- app/src/main/java/com/example/lifestyleapp_spring2023/MainActivity.kt
+++ app/src/main/java/com/example/lifestyleapp_spring2023/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lifestyleapp_spring2023
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 
@@ -10,10 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val redButton: Button = findViewById(R.id.prof_button)
+        redButton.setBackgroundColor(Color.RED)
+
+        val orangeButton: Button = findViewById(R.id.bmr_button)
+        orangeButton.setBackgroundColor(Color.rgb(255, 165, 0))
+
+        val yellowButton: Button = findViewById(R.id.weather_button)
+        yellowButton.setBackgroundColor(Color.YELLOW)
+
+        val greenButton: Button = findViewById(R.id.hikes_button)
+        greenButton.setBackgroundColor(Color.GREEN)
+
         //Find each frame layout, replace with corresponding fragment
-        val fTrans: FragmentTransaction = supportFragmentManager.beginTransaction()
-        fTrans.replace(R.id.fragmentFrame1, BMRPage(), "Frag_1")
-        fTrans.replace(R.id.fragmentFrame2, NavBar(), "Frag_2")
-        fTrans.commit()
+//        val fTrans: FragmentTransaction = supportFragmentManager.beginTransaction()
+//        fTrans.replace(R.id.fragmentFrame1, BMRPage(), "Frag_1")
+//        fTrans.replace(R.id.fragmentFrame2, NavBar(), "Frag_2")
+//        fTrans.commit()
     }
 }
