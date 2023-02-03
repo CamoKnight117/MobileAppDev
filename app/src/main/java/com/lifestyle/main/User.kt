73bfd1bda3b9de02f4bf329962fea9e9be3ee1d6
CAMERON KNIGHT <u1070175@umail.utah.edu>
2023-02-02 18:19:43 -0700

    Finished implementing the User.kt and ActivityLevel.kt classes

--- app/src/main/java/com/lifestyle/main/User.kt
+++ app/src/main/java/com/lifestyle/main/User.kt
@@ -9,28 +9,33 @@ class User() {
     var height = 0;
     var weight = 0;
     var sex = Sex.UNASSIGNED;
-    var activityLevel = "TODO";
+    var activityLevel = ActivityLevel();
     var profilePicture = "TODO";
+
+    enum class Sex
+    {
+        MALE, FEMALE, UNASSIGNED
+    }
+
+    fun getDailyCalorieIntake() : Float {
+        val bmrVal = calculateBMR()
+        return if(bmrVal != 0f) {
+            activityLevel.workoutCaloriesPerWeek()/7 + bmrVal;
+        } else {
+            0f;
+        }
+    }
+
+    fun calculateBMR() : Float {
+        return if(age != 0 && height != 0 && weight != 0 && sex != Sex.UNASSIGNED) {
+            when (sex) {
+                Sex.MALE -> 88.362f + (13.397f * weight) + (4.799f*height)-(5.677f*age)
+                Sex.FEMALE -> (447.593f + (9.247f * weight) + (3.098f * height)-(4.330f * age))
+                Sex.UNASSIGNED -> 0f
+            }
+        } else {
+            0f
+        }
+    }
 }
 
-enum class Sex
-{
-    MALE, FEMALE, UNASSIGNED
-}
-
-fun getLevel(): ActivityLevel.Level {
-    //TODO: Implement this method. Return level based on activity level
-    return ActivityLevel.Level.ACTIVE
-}
-
-fun getWeeklyCalorieIntake() {
-    //TODO: Implement this method.
-}
-
-fun getDailyCalorieIntake() {
-    //TODO: Implement this method.
-}
-
-fun caluclateBMR() {
-    //TODO: Implement this method.
-}
