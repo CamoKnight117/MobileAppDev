c00d4d89c4635345d25c6e7d6b1a065cc3eb80fe
Preston Hales <PrestongHales@gmail.com>
2023-02-09 16:30:42 -0700

    Add viewbinding

--- app/src/main/java/com/lifestyle/bmr/BMRPage.kt
+++ app/src/main/java/com/lifestyle/bmr/BMRPage.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lifestyleapp_spring2023.R
+import com.example.lifestyleapp_spring2023.databinding.FragmentBmrPageFragmentBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,7 +22,7 @@ class BMRPage : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private val binding = FragmentBmrPageFragmentBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -36,6 +37,11 @@ class BMRPage : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bmr_page_fragment, container, false)
+    }
+
+
+    fun updateApp() {
+
     }
 
     companion object {
