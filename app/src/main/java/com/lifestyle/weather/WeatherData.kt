bfb94cdd773a69f426e636522154c3f8c13afa11
S B <se.email.exe@gmail.com>
2023-04-11 18:59:46 -0600

    Update WeatherFragment to the viewmodel pattern

---
+++ app/src/main/java/com/lifestyle/weather/WeatherData.kt
@@ -0,0 +1,36 @@
+package com.lifestyle.weather
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/** A response from https://api.openweathermap.org/data/2.5/weather, minus "internal parameter"s, parsed from JSON.
+ * See https://openweathermap.org/current#parameter for details.
+ */
+@Serializable
+class WeatherData(
+    val coord: Coord? = null,
+    val weather: List<Weather>? = null,
+    val main: Main? = null,
+    val visibility: Double? = null,
+    val wind: Wind? = null,
+    val rain: Rain? = null,
+    val snow: Snow? = null,
+    val dt: Double? = null,
+    val sys: Sys? = null,
+    val timezone: Double? = null
+) {
+    @Serializable
+    public class Coord(val lon: Double? = null, val lat: Double? = null) { }
+    @Serializable
+    public class Weather(val id: Double? = null, val main: String? = null, val description: String? = null, val icon: String? = null) { }
+    @Serializable
+    public class Main(val temp: Double? = null, val feels_like: Double? = null, val temp_min: Double? = null, val temp_max: Double? = null, val pressure: Double? = null, val humidity: Double? = null, val sea_level: Double? = null, val grnd_level: Double? = null)
+    @Serializable
+    public class Wind(val speed: Double? = null, val deg: Double? = null, val gust: Double? = null) { }
+    @Serializable
+    public class Rain(@SerialName("1h") val oneH: Double? = null, @SerialName("3h") val threeH: Double? = null) { }
+    @Serializable
+    public class Snow(@SerialName("1h") val oneH: Double? = null, @SerialName("3h") val threeH: Double? = null) { }
+    @Serializable
+    public class Sys(val country: String? = null, val sunrise: Double? = null, val sunset: Double? = null) { }
+}
--- app/src/main/java/com/lifestyle/weather/WeatherFragment.kt
+++ app/src/main/java/com/lifestyle/weather/WeatherFragment.kt
@@ -15,17 +15,18 @@ import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.os.HandlerCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.lifestyle.R
 import com.lifestyle.main.MainActivity
 import com.lifestyle.main.UserProvider
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.net.URL
 import kotlin.concurrent.thread
 import kotlin.math.roundToInt
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,38 +44,7 @@ class WeatherFragment : Fragment() {
     private var weatherTextView: TextView? = null
     private var temperatureTextView: TextView? = null
     private var handler: Handler = HandlerCompat.createAsync(Looper.getMainLooper())
-
-    /** A response from https://api.openweathermap.org/data/2.5/weather, minus "internal parameter"s, parsed from JSON.
-     * See https://openweathermap.org/current#parameter for details.
-     */
-    @Serializable
-    private class OpenWeatherCurrentWeatherReply(
-        val coord: Coord? = null,
-        val weather: List<Weather>? = null,
-        val main: Main? = null,
-        val visibility: Double? = null,
-        val wind: Wind? = null,
-        val rain: Rain? = null,
-        val snow: Snow? = null,
-        val dt: Double? = null,
-        val sys: Sys? = null,
-        val timezone: Double? = null
-    ) {
-        @Serializable
-        public class Coord(val lon: Double? = null, val lat: Double? = null) { }
-        @Serializable
-        public class Weather(val id: Double? = null, val main: String? = null, val description: String? = null, val icon: String? = null) { }
-        @Serializable
-        public class Main(val temp: Double? = null, val feels_like: Double? = null, val temp_min: Double? = null, val temp_max: Double? = null, val pressure: Double? = null, val humidity: Double? = null, val sea_level: Double? = null, val grnd_level: Double? = null)
-        @Serializable
-        public class Wind(val speed: Double? = null, val deg: Double? = null, val gust: Double? = null) { }
-        @Serializable
-        public class Rain(@SerialName("1h") val oneH: Double? = null, @SerialName("3h") val threeH: Double? = null) { }
-        @Serializable
-        public class Snow(@SerialName("1h") val oneH: Double? = null, @SerialName("3h") val threeH: Double? = null) { }
-        @Serializable
-        public class Sys(val country: String? = null, val sunrise: Double? = null, val sunset: Double? = null) { }
-    }
+    private lateinit var weatherViewModel : WeatherViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -117,110 +87,47 @@ class WeatherFragment : Fragment() {
             }
         }
 
-        // Start a weather API request if needed.
-        sendWeatherRequestIfNeeded()
+        // Initialize the viewmodel and set an observer.
+        weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+        weatherViewModel.data.observe(this.viewLifecycleOwner) { weatherData : WeatherData ->
+            timestampLastWeatherReply = System.currentTimeMillis()
+            lastWeatherReply = weatherData
+            putWeatherOnUI()
+        }
+
+        putWeatherOnUI()
 
         return newView
     }
 
-    private fun sendWeatherRequestIfNeeded() {
-        // The minimum distance in meters a user's location must change before the weather should be re-queried.
-        val minWeatherRefreshDistance : Float = 10000f
-        val activity = requireActivity()
-        val location = userProvider?.getUser()?.location
-        val isLocationDifferent = (locationOnLastWeatherReply==null && location!=null)
-                || (locationOnLastWeatherReply!=null && location!=null && location.distanceTo(locationOnLastWeatherReply!!) > minWeatherRefreshDistance+locationOnLastWeatherReply!!.accuracy+location.accuracy)
-        if((lastWeatherCallThread?.isAlive != true)
-            || System.currentTimeMillis() - timestampLastWeatherReply >= weatherRefreshIntervalMillis
-            || isLocationDifferent
-        ) {
-            if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-                sendWeatherRequest(handler)
-            } else {
-                val requestCode = MainActivity.registerPermissionRequestCallback(object : MainActivity.Companion.PermissionRequestCallback {
-                    override fun invoke(activity: Activity, permissions: Array<out String>, grantResults: IntArray) {
-                        sendWeatherRequest(handler)
-                    }
-                })
-                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.INTERNET), requestCode)
-            }
-        }
-    }
-
-    private fun sendWeatherRequest(handler: Handler) {
-        // Do not create more than one weather request thread at once.
-        if(lastWeatherCallThread?.isAlive == true)
-            return
-        lastWeatherCallThread = thread() {
-            var weatherText: String?
-            var temperatureText: String?
-            val userProviderVal = userProvider
-            if(userProviderVal == null) {
-                handler.post {
-                    this.weatherTextView?.text     = getString(R.string.weatherErrorMessage)
-                    this.temperatureTextView?.text = null
-                }
-                return@thread
-            }
-            val user = userProviderVal.getUser()
-            val location = user.location
-            if(location == null) {
-                handler.post {
-                    this.weatherTextView?.text     = null
-                    this.temperatureTextView?.text = null
-                }
-                return@thread
-            }
-            val response: String = URL("https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=cd31a8658a4169b5b89342953b4f940b").readText() // .openConnection() as HttpURLConnection //URL("https", "api.openweathermap.org", -1, "/data/2.5/weather?lat=0&lon=0&appid=${INSECURE_OPENWEATHER_KEY}").openConnection()
-            try {
-                val weather = Json{ignoreUnknownKeys=true}.decodeFromString<OpenWeatherCurrentWeatherReply>(response)
-                handler.post {
-                    timestampLastWeatherReply = System.currentTimeMillis()
-                    lastWeatherReply = weather
-                    putWeatherOnUI()
-                }
-            } catch(e: SerializationException) {
-                handler.post {
-                    this.weatherTextView?.text     = getString(R.string.weatherErrorMessage)
-                    this.temperatureTextView?.text = null
-                }
-            } catch(e: java.lang.IllegalArgumentException) {
-                handler.post {
-                    this.weatherTextView?.text     = getString(R.string.weatherErrorMessage)
-                    this.temperatureTextView?.text = null
-                }
-            }
-        }
-    }
-
     private fun putWeatherOnUI() {
-        val weatherText     = lastWeatherReply?.weather?.getOrNull(0)?.main ?: getString(R.string.weatherErrorMessage)
-        val temperatureKelvin = lastWeatherReply?.main?.temp
-        val temperatureText: String? = if(temperatureKelvin != null) {
-            val temperatureFarenheit: Double = (temperatureKelvin - 273.15) * 9.0/5.0 + 32.0
-            getString(R.string.temperatureFarenheit, temperatureFarenheit.roundToInt())
-        } else
-            getString(R.string.temperatureErrorMessage)
+        var weatherText     = ""
+        var temperatureText = ""
+        lastWeatherReply?.apply{
+            weatherText = weather?.getOrNull(0)?.main ?: getString(R.string.weatherErrorMessage)
+            val temperatureKelvin = lastWeatherReply?.main?.temp
+            temperatureText = if(temperatureKelvin != null) {
+                val temperatureFarenheit: Double = (temperatureKelvin - 273.15) * 9.0/5.0 + 32.0
+                getString(R.string.temperatureFarenheit, temperatureFarenheit.roundToInt())
+            } else
+                getString(R.string.temperatureErrorMessage)
+        }
         this.weatherTextView?.text     = weatherText
         this.temperatureTextView?.text = temperatureText
     }
 
     private fun onLocationUpdated() {
         locationTextView?.text = userProvider?.getUser()?.locationName ?: getString(R.string.none)
-        sendWeatherRequestIfNeeded()
+        userProvider?.getUser()?.location?.let { weatherViewModel.setLocation(it) }
     }
 
     companion object {
-        /** The unsecured key to our team's OpenWeather API access. This key provides API access, not account access.
-         * Our account is free-tier; it has no billing info attached.
-         * Our account has access to 60 calls per minute, to a maximum of 1,000,000 calls per month. */
-        private const val INSECURE_OPENWEATHER_KEY = "cd31a8658a4169b5b89342953b4f940b"
         // Weather query rate limiting is managed over the lifetime of the app by static fields:
         private const val weatherRefreshIntervalMillis = 1000 * 60 * 60
         private var lastWeatherCallThread: Thread? = null
         private var timestampLastWeatherReply: Long = 0
         private var locationOnLastWeatherReply : Location? = null
-        private var lastWeatherReply: OpenWeatherCurrentWeatherReply? = null
+        private var lastWeatherReply: WeatherData? = null
 
         /**
          * Use this factory method to create a new instance of
